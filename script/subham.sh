8c25e775a4411f04c6a5d334195f692c51c6c6aa
Subham Das <57589556+dsubham76@users.noreply.github.com>
2020-12-20 21:33:57 +0530

    Update subham.sh

--- script/subham.sh
+++ script/subham.sh
@@ -67,6 +67,11 @@ playonlinux(){
 	sudo apt update
 	sudo apt-get install playonlinux
 }
+ubuntu_cleaner(){
+	sudo add-apt-repository ppa:gerardpuig/ppa
+	sudo apt-get update
+	sudo apt-get install Ubuntu-cleaner
+}
 sublimetext(){
 	wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 	echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
