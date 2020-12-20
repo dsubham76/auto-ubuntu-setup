b96930d8e85af3d13de9b8c5c9c0cab835b65a1e
Subham Das <57589556+dsubham76@users.noreply.github.com>
2020-12-20 21:32:37 +0530

    Update subham.sh

--- script/subham.sh
+++ script/subham.sh
@@ -59,6 +59,14 @@ midnight_commander(){
 	sudo apt update
 	sudo apt install mc
 }
+tlp(){
+	sudo apt install tlp tlp-rdw
+	sudo tlp start
+}
+playonlinux(){
+	sudo apt update
+	sudo apt-get install playonlinux
+}
 sublimetext(){
 	wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 	echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
